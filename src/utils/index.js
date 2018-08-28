0a9a6e13cd26b9504111ff766606ae9235d438b5
Yujin Ariza <yujin.ariza@gmail.com>
2018-08-27 22:03:26 -0400

    clean up stuff

--- src/utils/index.js
+++ src/utils/index.js
@@ -15,3 +15,24 @@ function is_touch_device() {
 }
 
 export const isTouch = is_touch_device();
+
+export const smoothdamp = (current, target, currentVelocity, smoothTime, maxSpeed, deltaTime) => {
+    smoothTime = max (0.000, smoothTime);
+    let num = 2 / smoothTime;
+    let num2 = num * deltaTime;
+    let num3 = 1 / (1 + num2 + 0.48 * num2 * num2 + 0.235 * num2 * num2 * num2);
+    let num4 = current - target;
+    let num5 = target;
+    let num6 = maxSpeed * smoothTime;
+    num4 = clamp (num4, -num6, num6);
+    target = current - num4;
+    let num7 = (currentVelocity + num * num4) * deltaTime;
+    currentVelocity = (currentVelocity - num * num7) * num3;
+    let num8 = target + (num4 + num7) * num3;
+    if (num5 - current > 0 === num8 > num5)
+    {
+        num8 = num5;
+        currentVelocity = (num8 - num5) / deltaTime;
+    }
+    return [num8, currentVelocity];
+};
--- src/waves/index.js
+++ src/waves/index.js
@@ -1,6 +1,5 @@
 import Sketch from '../sketch';
 import * as THREE from 'three';
-let OrbitControls = require('three-orbit-controls')(THREE);
 import waveVert from './shaders/wave.vert';
 import waveFrag from './shaders/wave.frag';
 import waveVertLowQ from './shaders/wave-mobile.vert';
@@ -15,6 +14,7 @@ export default class Waves {
     constructor(options) {
 
         this.renderer = new THREE.WebGLRenderer({
+            // antialias: true,
             alpha: true,
         });
         this.renderer.autoClear = true;
@@ -32,27 +32,6 @@ export default class Waves {
         this.ctx.draw = this.draw.bind(this);
         this.ctx.mousemove = this.mousemove.bind(this);
         this.ctx.update = this.update.bind(this);
-    }
-
-    smoothdamp = (current, target, currentVelocity, smoothTime, maxSpeed, deltaTime) => {
-        smoothTime = max (0.000, smoothTime);
-        let num = 2 / smoothTime;
-        let num2 = num * deltaTime;
-        let num3 = 1 / (1 + num2 + 0.48 * num2 * num2 + 0.235 * num2 * num2 * num2);
-        let num4 = current - target;
-        let num5 = target;
-        let num6 = maxSpeed * smoothTime;
-        num4 = clamp (num4, -num6, num6);
-        target = current - num4;
-        let num7 = (currentVelocity + num * num4) * deltaTime;
-        currentVelocity = (currentVelocity - num * num7) * num3;
-        let num8 = target + (num4 + num7) * num3;
-        if (num5 - current > 0 === num8 > num5)
-        {
-            num8 = num5;
-            currentVelocity = (num8 - num5) / deltaTime;
-        }
-        return [num8, currentVelocity];
     }
 
     setup = () => {
